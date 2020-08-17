102cbf8493f8404f13820e06b553fc7175bcabca
jdvolk <justin.d.volk@gmail.com>
2020-08-17 07:43:57 -0600

    add filter link component

---
+++ src/containers/FilterLink.js
@@ -0,0 +1,15 @@
+import { connect } from 'react-redux';
+import { setVisibilityFilter } from '../actions';
+import Link from '../components/Link';
+
+const  mapStateToProps = (state, props) => ({
+    active: props.filter === state.visibilityFilter
+});
+
+const mapDispatchToProps = (dispatch, props) => ({
+    onCLick: () => dispatch(setVisibilityFilter(props.filter))
+});
+
+const FilterLink = connect(mapStateToProps, mapDispatchToProps)(Link);
+
+export default FilterLink;
